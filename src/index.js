f722538d6d486004b2930d15a416fe75ebaa5b64
Andrei <aalisenak@gmail.com>
2020-01-23 18:50:02 +0300

    add Fetch API

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 class SoccerService {
 
-    _apiBase = 'https://api.sportradar.us';
+    _apiBase = 'https://swapi.co/api';
 
     async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-        // if (!res.ok) {
-        //     throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
-        // }
+        const res = await fetch(
+            `${this._apiBase}${url}`
+            // {headers: {'X-Auth-Token': 'bef879d6107c495b8a2bf9d4b06e24db'}}
+        );
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`)
+        }
         return await res.json();
     }
 
-    async getAllClubs() {
-        return this.getResource(`/soccer-extended/`);
+    async getAllNames() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
 
+    async getName(id) {
+        return await this.getResource(`/people/${id}`);
     }
 }
 
 const scoreBat = new SoccerService();
 
-scoreBat.getAllClubs().then((body) => {
-    console.log(body);
+
+scoreBat.getName(7).then((names) => {
+    // names.forEach((n) => {
+    //     console.log(n.name);
+    // })
+    console.log(names.name);
+
 });
-
-
-
-
-
-
